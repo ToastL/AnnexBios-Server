59a94a0485e29fa0d76d1716ffce9abbb9a48a56
ToastL <etiendenouden2006@gmail.com>
2024-09-10 11:38:27 +0200

    compile to public folder

---
+++ public/types/requests.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Request = void 0;
+let Request = exports.Request = /*#__PURE__*/function (Request) {
+  Request[Request["GET"] = 0] = "GET";
+  Request[Request["POST"] = 1] = "POST";
+  Request[Request["GET_POST"] = 2] = "GET_POST";
+  return Request;
+}({});
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6WyJSZXF1ZXN0IiwiZXhwb3J0cyJdLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy90eXBlcy9yZXF1ZXN0cy50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgZW51bSBSZXF1ZXN0IHtcbiAgICBHRVQsXG4gICAgUE9TVCxcbiAgICBHRVRfUE9TVFxufSJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0lBQWtCQSxPQUFPLEdBQUFDLE9BQUEsQ0FBQUQsT0FBQSwwQkFBUEEsT0FBTztFQUFQQSxPQUFPLENBQVBBLE9BQU87RUFBUEEsT0FBTyxDQUFQQSxPQUFPO0VBQVBBLE9BQU8sQ0FBUEEsT0FBTztFQUFBLE9BQVBBLE9BQU87QUFBQSIsImlnbm9yZUxpc3QiOltdfQ==
