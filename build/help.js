a490ba81bceaff788615445ca00e0fbd8e54c30a
Toaster5 <105885246+ToastL@users.noreply.github.com>
2024-09-10 11:43:51 +0200

    Delete build directory

--- build/help.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sInNvdXJjZXMiOlsiLi4vc3JjL2hlbHAudHMiXSwic291cmNlc0NvbnRlbnQiOlsiIl0sIm1hcHBpbmdzIjoiIiwiaWdub3JlTGlzdCI6W119
