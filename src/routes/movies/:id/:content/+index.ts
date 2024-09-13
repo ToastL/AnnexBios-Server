a180a4324b7e273aded6515ca4fa3bafd9ac2ebd
ToastL <etiendenouden2006@gmail.com>
2024-09-13 14:36:46 +0200

    Error handling

--- src/routes/movies/:id/:content/+index.ts
+++ src/routes/movies/:id/:content/+index.ts
@@ -7,18 +7,22 @@ export const request: Request = Request.GET
 
 export const callback = (pgConn: Client): RequestHandler => {
     return async (req, res) => {
-        let result = await pgConn.query(`SELECT movie.${req.params.content} from (SELECT m.*, 
-                    ARRAY_AGG(a.* ORDER BY a.id, ', ') AS actor_ids 
-                    FROM movies m 
-                    JOIN movie_actors ma ON m.id = ma.movie_id
-                    JOIN actors a ON a.id = ma.actor_id
-                    WHERE m.id = ${req.params.id}
-                    GROUP BY m.id, m.title) as movie;`)
-        if (result.rows.length == 0) {
-            res.status(202).send({message: `movie with id '${req.params.id}' does not exist`})
-            return
+        try {
+            let result = await pgConn.query(`SELECT movie.${req.params.content} from (SELECT m.*, 
+                        ARRAY_AGG(a.* ORDER BY a.id, ', ') AS actor_ids 
+                        FROM movies m 
+                        JOIN movie_actors ma ON m.id = ma.movie_id
+                        JOIN actors a ON a.id = ma.actor_id
+                        WHERE m.id = ${req.params.id}
+                        GROUP BY m.id, m.title) as movie;`)
+            if (result.rows.length == 0) {
+                res.status(202).send({message: `movie with id '${req.params.id}' does not exist`})
+                return
+            }
+    
+            res.send(result.rows)
+        } catch(e) {
+            res.send(e)
         }
-
-        res.send(result.rows)
     }
 }
